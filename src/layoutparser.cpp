8101c7363c4f9e3650d0ca917e46841f69ec7f65
papparazzo <pappi-@gmx.de>
2023-05-09 16:36:59 +0200

    wip...

--- src/layoutparser.cpp
+++ src/layoutparser.cpp
@@ -20,9 +20,183 @@
 
 #include "layoutparser.h"
 
-LayoutParser::LayoutParser() {
+#include <iostream>
+#include <memory>
+
+#include "layoutparser.h"
+#include "node_block.h"
+#include "node_crossoverswitch.h"
+#include "node_threewayswitch.h"
+#include "node_simpleswitch.h"
+#include "moba/shared.h"
+#include <cassert>
+
+void LayoutParser::fetchBlockNodes(Direction curDir, Position curPos) {
+    auto startDir = curDir;
+    auto startPos = curPos;
+
+    //std::cout << std::endl << "start fetch routine" << std::endl;
+    while(true) {
+        // Schritt weiter zum nächsten Symbol
+        curPos.setNewPosition(curDir);
+
+        // Symbol von der aktuellen Position im Gleisplan
+        auto curSymbol = layout->get(curPos);
+        auto compDir = curDir.getComplementaryDirection();
+
+        assert((curSymbol) && "invalid layout: No symbol at current position");
+        assert((curSymbol->symbol.isJunctionSet(compDir)) && "invalid layout: no open junctions");
+
+        curSymbol->symbol.removeJunction(compDir);
+
+        if(curSymbol->symbol.isEnd()) {
+            //std::cout << "Bumper connected. Leaving..." << std::endl << std::endl;
+            return;
+        }
+
+        auto block = blockContacts->find(curPos);
+        auto iters = switchstates->find(curPos);
+
+        /*
+        std::cout <<
+            curPos << " -> " <<
+            (block == blockContacts->end() ? "" : " B ") <<
+            (iters == switchstates->end() ? "" : " S ") << std::endl;
+         */
+
+        // Prüfen, ob Symbol keine Weiche und kein Block ist
+        if(block == blockContacts->end() && iters == switchstates->end()) {
+            // Wenn Symbol gerades Gleis oder Kreuzung dann einfach weiter in Richtung
+            if(curSymbol->symbol.isBend()) {
+                curDir = curSymbol->symbol.getNextOpenJunction(curDir);
+            }
+            continue;
+        }
+
+        // Ab hier entweder Weiche oder Block! Startknoten ermitteln...
+        auto &startNode = nodes[startPos];
+
+        // Existiert an aktueller Stelle schon ein Knoten?
+        auto iter = nodes.find(curPos);
+        if(iter != nodes.end()) {
+            //... wenn ja, diesen mit dem Startknoten verbinden und Funktion verlassen
+            startNode.junctions[startDir](iter->second.node);
+            iter->second.junctions[compDir](startNode.node);
+            curSymbol->symbol.removeJunction(curDir);
+            //std::cout << "Node connected. Leaving..." << std::endl << std::endl;
+            return;
+        }
+
+        auto &curNode = nodes[curPos];
+        NodePtr newNode;
+        Symbol sym;
+
+        if(block != blockContacts->end()) {
+            // ...aktueller Knoten ist ein Block
+            auto bNode = createBlock(curSymbol->id, block->second);
+            sym = curSymbol->symbol;
+            sym.reset();
+            (*blockNodeMap)[block->second->blockContact] = bNode;
+            newNode = bNode;
+        } else {
+            // ...aktueller Knoten ist eine Weiche
+            assert(iters != switchstates->end() && "no switch state set");
+
+            if(curSymbol->symbol.isLeftSwitch()) {
+                sym = Symbol{Symbol::LEFT_SWITCH};
+                newNode = std::make_shared<SimpleSwitch>(curSymbol->id, iters->second.switchStand);
+            } else if(curSymbol->symbol.isRightSwitch()) {
+                sym = Symbol{Symbol::RIGHT_SWITCH};
+                newNode = std::make_shared<SimpleSwitch>(curSymbol->id, iters->second.switchStand);
+            } else if(curSymbol->symbol.isCrossOverSwitch()) {
+                sym = Symbol{Symbol::CROSS_OVER_SWITCH};
+                newNode = std::make_shared<CrossOverSwitch>(curSymbol->id, iters->second.switchStand);
+            } else if(curSymbol->symbol.isThreeWaySwitch()) {
+                sym = Symbol{Symbol::THREE_WAY_SWITCH};
+                newNode = std::make_shared<ThreeWaySwitch>(curSymbol->id, iters->second.switchStand);
+            }
+            (*switcheNodeMap)[iters->second.id] = newNode;
+        }
+
+        curNode.node = newNode;
+        auto offset = curSymbol->symbol.getDistance(sym);
+
+        startNode.junctions[startDir](curNode.node);
+
+        Direction dir;
+
+        while((dir = sym.getNextOpenJunction()) != Direction::UNSET) {
+            curNode.junctions[dir + offset] = [newNode, dir](const NodePtr &nptr) {newNode->setJunctionNode(dir, nptr);};
+            sym.removeJunction(dir);
+        }
+
+        sym.reset();
+
+        while((dir = sym.getNextOpenJunction()) != Direction::UNSET) {
+            sym.removeJunction(dir);
+            if(dir + offset == compDir) {
+                curNode.junctions[compDir](startNode.node);
+                continue;
+            }
+            
+            // Pruefen, ob Junction noch existiert. Bei eine Kehrschleife kann
+            // diese durch Rekursion bereits geloescht worden sein
+            if(curSymbol->symbol.removeJunction(dir + offset)) {
+                fetchBlockNodes(dir + offset, curPos);
+            }
+        }
+        //std::cout << "No open junctions. Leaving..." << std::endl << std::endl;
+        return;
+    }
 }
 
-LayoutParser::~LayoutParser() {
+void LayoutParser::parse(LayoutContainerPtr layout, BlockContactDataMapPtr blockContacts, SwitchStandMapPtr switchstates, TrainListPtr trainList) {
+
+    this->layout = layout;
+    this->blockContacts = blockContacts;
+    this->switchstates = switchstates;
+    this->trainList = trainList;
+
+    auto firstBlockContact = blockContacts->begin();
+    if(firstBlockContact == blockContacts->end()) {
+        throw LayoutParserException{"no blocks found"};
+    }
+
+    Position pos = firstBlockContact->first;
+
+    auto curSymbol = layout->get(pos);
+    auto dir1 = curSymbol->symbol.getNextJunction();
+    auto dir2 = curSymbol->symbol.getNextJunction(dir1);
+    auto firstBlockContactData = firstBlockContact->second;
+
+    auto block = createBlock(curSymbol->id, firstBlockContactData);
+    auto &tmp = nodes[pos];
+
+    (*blockNodeMap)[firstBlockContactData->blockContact] = block;
+
+    tmp.node = block;
+    tmp.junctions[dir1] = [dir1, block](const NodePtr &nptr) {block->setJunctionNode(dir1, nptr);};
+    tmp.junctions[dir2] = [dir2, block](const NodePtr &nptr) {block->setJunctionNode(dir2, nptr);};
+
+    curSymbol->symbol.removeJunction(dir1);
+    fetchBlockNodes(dir1, pos);
+
+    if(!curSymbol->symbol.isJunctionSet(dir2)) {
+        return;
+    }
+
+    curSymbol->symbol.removeJunction(dir2);
+    fetchBlockNodes(dir2, pos);
 }
 
+BlockPtr LayoutParser::createBlock(int id, BlockContactDataPtr contact) {
+    // get train from block and set it
+    auto iter = trainList->find(contact->trainId);
+    auto block = std::make_shared<Block>(id);
+
+    if(iter != trainList->end()) {
+        block->setTrain(iter->second);
+    }
+
+    return block;
+}
--- src/layoutparser.h
+++ src/layoutparser.h
@@ -20,11 +20,78 @@
 
 #pragma once
 
-class LayoutParser {
-public:
-    LayoutParser();
-    virtual ~LayoutParser();
-private:
+#include <map>
+#include <exception>
 
+#include "node.h"
+#include "moba/position.h"
+#include "moba/container.h"
+#include "moba/symbol.h"
+#include "moba/direction.h"
+#include "moba/shared.h"
+#include "common.h"
+#include <functional>
+
+class LayoutParserException: public std::exception {
+    public:
+        explicit LayoutParserException(const std::string &err) throw() : what_{err} {
+        }
+
+        LayoutParserException() throw() : what_{"Unknown error"} {
+        }
+
+        virtual ~LayoutParserException() throw() {
+        }
+
+        virtual const char *what() const throw() {
+            return this->what_.c_str();
+        }
+
+    private:
+        std::string what_;
+};
+
+class LayoutParser {
+
+    using NodeCallback = std::function<void(const NodePtr&)>;
+
+    // In
+    LayoutContainerPtr layout;
+    BlockContactDataMapPtr blockContacts;
+    SwitchStandMapPtr switchstates;
+    TrainListPtr trainList;
+
+    // Out
+    SwitchNodeMapPtr switcheNodeMap;
+    BlockNodeMapPtr blockNodeMap;
+
+    struct NodeJunctions {
+        std::map<Direction, NodeCallback> junctions;
+        NodePtr node;
+    };
+
+    std::map<Position, NodeJunctions> nodes;
+
+    void fetchBlockNodes(Direction dir, Position pos);
+    BlockPtr createBlock(int id, BlockContactDataPtr contact);
+
+public:
+    LayoutParser() {
+        switcheNodeMap = std::make_shared<std::map<int, std::shared_ptr<Node>>>();
+        blockNodeMap = std::make_shared<std::map<ContactData, std::shared_ptr<Block>>>();
+    }
+
+    virtual ~LayoutParser(){
+    }
+
+    void parse(LayoutContainerPtr layout, BlockContactDataMapPtr blockContacts, SwitchStandMapPtr switchstates, TrainListPtr trainList);
+
+    SwitchNodeMapPtr getSwitchMap() const {
+        return switcheNodeMap;
+    }
+
+    BlockNodeMapPtr getBlockMap() const {
+        return blockNodeMap;
+    }
 };
 
